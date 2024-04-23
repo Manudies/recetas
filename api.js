415811c574382dbdc110208857a50a2ba7be10ab
Manudies <manudies@gmail.com>
2024-04-23 23:25:26 +0200

    feat: Buscador y estilos

--- api.js
+++ api.js
@@ -1,12 +1,13 @@
 import {myKey, myID} from "./apikey.js";
 
 async function recetas(busqueda){
-    const url = new URL(`https://api.edamam.com/api/recipes/v2?type=public&q=${busqueda}&app_id=${myID}&app_key=${myKey}`)
-    console.log(url)
+    const url = new URL(`https://api.edamam.com/api/recipes/v2?type=public&q=${busqueda}&app_id=${myID}&app_key=${myKey}`);
+    // console.log(url)
     try {
         const response = await fetch(url.toString());
         const data = await response.json();
-        console.log(data);
+        // console.log(data)
+        return data;
     }
     catch (error) {
         console.error(error);
--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 import { recetas } from "./api.js"
 
-// recetas("pollo")
+//Creamos el buscador y llamamos a la API
+const inputBusqueda = document.getElementById("input_buscador")
+const boton = document.getElementById("boton_busqueda")
+boton.addEventListener("click", async () => {
+    try {
+        const resultadoAPI = await recetas(inputBusqueda.value.trim());
+        console.log(resultadoAPI)
+        return AddRecetas(resultadoAPI.hits)
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+//Creamos los elementos necesarios para mostrar los datos
+function AddRecetas(recetas) {
+    const contenedorRecetas = document.querySelector(".recetas");
+    contenedorRecetas.innerHTML = "";
+
+    const pResultado = document.createElement("p");
+    pResultado.setAttribute("class", "p_resultado");
+    pResultado.setAttribute("id", "p_resultado");
+    pResultado.innerText = "Aquí tienes algunas sugerencias";
+    contenedorRecetas.appendChild(pResultado);
+
+    if (Array.isArray(recetas)) {
+        recetas.forEach((receta, index) => {
+        // console.log(index)
+        const contenedorReceta = document.createElement("div");
+        contenedorReceta.setAttribute("class", "contenedorReceta");
+        contenedorReceta.setAttribute("id", "contenedorReceta" + index);
+
+        const nombre = document.createElement("h1");
+        nombre.setAttribute("class", "nombre");
+        nombre.setAttribute("id", "nombre" + index);
+        console.log(receta.recipe.label);
+        nombre.textContent = receta.recipe.label;
+
+        // Añade más elementos y configura según sea necesario para cada receta
+
+        contenedorReceta.appendChild(nombre);
+        contenedorRecetas.appendChild(contenedorReceta);
+    });
+    } else {
+        console.error("La variable 'recetas' no es un array.");
+    }
+    
+    
+
+}
+
