c8ff2a55d22d9926478eaea7d23da0cdca4bbfff
Manudies <manudies@gmail.com>
2024-04-24 01:18:37 +0200

    feat:mas datos y estructura de html

--- index.js
+++ index.js
@@ -15,32 +15,61 @@ boton.addEventListener("click", async () => {
 
 //Creamos los elementos necesarios para mostrar los datos
 function AddRecetas(recetas) {
-    const contenedorRecetas = document.querySelector(".recetas");
-    contenedorRecetas.innerHTML = "";
-
+    //Limpiamos la Seccion de las recetas
+    const secionRecetas = document.querySelector(".recetas");
+    secionRecetas.innerHTML = "";
+    //Agregamos párrafo de Sugerencias
     const pResultado = document.createElement("p");
     pResultado.setAttribute("class", "p_resultado");
     pResultado.setAttribute("id", "p_resultado");
-    pResultado.innerText = "Aquí tienes algunas sugerencias";
-    contenedorRecetas.appendChild(pResultado);
+    //Manejamos un input vacio
+    console.log(recetas.length)
+    if (recetas.length > 0){
+        pResultado.innerText = "Aquí tienes algunas sugerencias";
+    }
+    else {
+        pResultado.innerText = "Sin datos no hay receta";
+    }
+    //Creamos contenedor global de recetas
+    const contenedorRecetas = document.createElement("div");
+    contenedorRecetas.setAttribute("class", "contenedor_recetas");
+    contenedorRecetas.setAttribute("id", "contenedor_recetas");
+    //Agregamos los elementos creados
+    secionRecetas.appendChild(pResultado);
+    secionRecetas.appendChild(contenedorRecetas);
 
     if (Array.isArray(recetas)) {
         recetas.forEach((receta, index) => {
-        // console.log(index)
+        //Ficha de individual de cada receta
         const contenedorReceta = document.createElement("div");
         contenedorReceta.setAttribute("class", "contenedorReceta");
         contenedorReceta.setAttribute("id", "contenedorReceta" + index);
-
+        //Creamos contendor 1
+        const contenedor1 = document.createElement("div");
+        contenedor1.setAttribute("class", "contenedor1");
+        contenedor1.setAttribute("id", "contenedor1");
+        //Titulo
         const nombre = document.createElement("h1");
         nombre.setAttribute("class", "nombre");
         nombre.setAttribute("id", "nombre" + index);
-        console.log(receta.recipe.label);
-        nombre.textContent = receta.recipe.label;
-
+        nombre.innerText = receta.recipe.label;
+        //Imagen 
+        const foto = document.createElement("img");
+        foto.setAttribute("class", "foto");
+        foto.setAttribute("id", "foto" + index);
+        foto.setAttribute("src", receta.recipe.image)
+        //Etiquetas de salud
+        const salud = document.createElement("p");
+        salud.setAttribute("class", "salud");
+        salud.setAttribute("id", "salud" + index);
+        salud.innerText = receta.recipe.healthLabels
         // Añade más elementos y configura según sea necesario para cada receta
 
-        contenedorReceta.appendChild(nombre);
         contenedorRecetas.appendChild(contenedorReceta);
+        contenedorReceta.appendChild(contenedor1)
+        contenedor1.appendChild(nombre);
+        contenedor1.appendChild(foto);
+        contenedor1.appendChild(salud)
     });
     } else {
         console.error("La variable 'recetas' no es un array.");
